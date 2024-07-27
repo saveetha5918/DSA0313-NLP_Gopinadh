47584a09525616a54a5ea15c0c67102cad274539
M.Gopinadh <129883340+saveetha5918@users.noreply.github.com>
2024-07-27 08:06:56 +0530

    Update 16 spacy ner.py

--- 16 spacy ner.py
+++ 16 spacy ner.py
@@ -1,6 +1,11 @@
-
 import spacy
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 text = "Apple is looking at buying U.K. startup for $1 billion"
 doc = nlp(text)
 for ent in doc.ents:
